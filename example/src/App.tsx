561ba9fa8785ebdb292dff2f4e74c20cb769ded9
Sam Autrey <34108400+swushi@users.noreply.github.com>
2021-03-03 16:30:11 -0600

    chore: setup example dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import ReanimatedBottomsheet from 'react-native-reanimated-bottomsheet';
+import BottomSheet from 'react-native-reanimated-bottomsheet';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    ReanimatedBottomsheet.multiply(3, 7).then(setResult);
+    BottomSheet.multiply(3, 7).then(setResult);
   }, []);
 
   return (
